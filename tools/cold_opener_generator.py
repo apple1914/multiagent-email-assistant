2af1a44bf2a35711381b0b22208976c72ca34b22
apple1914 <lionpay7@mail.ru>
2024-06-20 15:58:24 +0500

    latest

--- tools/cold_opener_generator.py
+++ tools/cold_opener_generator.py
@@ -17,7 +17,7 @@ def generate_cold_opener(company_description:str):
     prompt_text = f"You are an expert at writing cold emails. Draft a perfect cold email for the company whose descrpition is provided in the context: <<<context: {context}>>> Your answer should be 2 sentence long, and should give a compliment relevant to the context"
 
     messages = [{"role":"system","content":prompt_text}]
-    client = OpenAI(api_key="sk-asdf-MkRgagrNycwmFmr1GjjgT3BlbkFJfKklQh0NsVMxUMkf9S8B")
+    client = OpenAI()
     response = client.chat.completions.create(
         model="gpt-4o",
         messages=messages,
